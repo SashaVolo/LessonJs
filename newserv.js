6d9c6efae1df89e9c7a78a11cad9df9811f21638
Volga <volgakokos@gmail.com>
2025-09-27 00:12:41 +0300

    added query and route params

--- newserv.js
+++ newserv.js
@@ -46,10 +46,62 @@ const fs = require("fs")
 const path = require("path")
 const pat = path.join(__dirname,"posts.json")
 const posts =JSON.parse(fs.readFileSync(pat,"utf8"))
-console.log(posts)
+let postsCopyAfterFiler=[...posts]
+console.log(posts[0].id)
 
 app.get("/posts", (req,res) =>{
-    res.json(posts)
+    //res.json(posts)
+    const take = req.query.take
+    const skip = req.query.skip
+    const filter = req.query.filter
+    if(filter){
+        let boolFilter;
+        if (filter === "true") boolFilter = true;
+        else if (filter === "false") boolFilter = false;
+        else {
+            res.status(400).json("query filter isn't a bool");
+            return;
+        }
+        if(boolFilter) postsCopyAfterFilter=posts.filter(post => post.name.includes('a'))
+    }
+    let postsCopyAfterSkip=[...postsCopyAfterFilter]
+    if(skip){
+        const numSkip = Number(skip)
+        if(isNaN(numSkip)){
+            res.status(400).json("query skip isn`t a number")
+            return
+        }
+        postsCopyAfterSkip = postsCopyAfterFilter.slice(numSkip)
+    }
+    let postsCopyAfterTake=[...postsCopyAfterSkip]
+    if(take){
+        const numTake = Number(take)
+        if(isNaN(numTake)){
+            res.status(400).json("query take isn`t a number")
+            return
+        }
+        console.log("y5jjtut")
+        postsCopyAfterTake = postsCopyAfterSkip.slice(0,numTake)
+    }
+    
+    res.status(200).json(postsCopyAfterTake)
+})
+
+app.get("/posts/:id",(req,res)=>{
+    const id = Number(req.params.id)
+    if(isNaN(id)){
+        console.log(id)
+        res.status(400).json("id isn`t a number")
+        return
+    }
+    const findPost=posts.find(post=>post.id ===id)
+    if(findPost!=null){
+        res.status(200).json(findPost)
+    }
+    else{
+        res.status(404).json("not found post")
+    }
+
 })
 
 app.listen(PORT, HOST, () => {
